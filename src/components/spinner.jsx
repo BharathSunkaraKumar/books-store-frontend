9349f4f0bf76d76eec0dba6aeac1535bbaa7ed57
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-08-08 19:47:36 +0530

    book-fronend

---
+++ src/components/spinner.jsx
@@ -0,0 +1,6 @@
+const RingSpinner = () => (
+  <div className="flex justify-center items-center">
+    <div className="animate-spin rounded-full h-8 w-8 border-2 border-gray-200 border-t-blue-500 "></div>
+  </div>
+);
+export default RingSpinner
